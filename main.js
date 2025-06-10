50f180315033009ed1a5394c4e4cfe6d56cd58b1
LaylaWolfsS <laylawolfss@gmail.com>
2025-06-10 10:30:30 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -6,17 +6,17 @@ let d = {
 }  
 function incNum1() {
     d.Num1 += d.Num1pc;
-    document.getElementById("Num1").innerHTML = Num1 + "Num1";
+    document.getElementById("Num1").innerHTML = Num1.toFixed(2) + "Num1";
 }
 function buyu1() {
     if(Num1 >= u1c) {
-        d.Num1 = d.Num1.minus(u1c).toFixed(2);
-        d.Num1pc = d.Num1pc.times(1.1).toFixed(2);
-        d.u1c = d.u1c.times(1.2).toFixed(2);
-        d.u1 = d.u1.plus(1).toFixed(2);
-        document.getElementById("u1a").innerHTML = "Level " + d.u1;
-        document.getElementById("Num1").innerHTML = d.Num1 + "Num1";
-        document.getElementById("u1").innerHTML = "Accelerate Num1 (Currently " + d.Num1pc + "/click) Cost:" + d.u1c + "Num1";
+        d.Num1 -= d.u1c;
+        d.Num1pc *= 1.1;
+        d.u1c *= 1.2;
+        d.u1++;
+        document.getElementById("u1a").innerHTML = "Level " + d.u1.toFixed(2);
+        document.getElementById("Num1").innerHTML = d.Num1.toFixed(2) + " Num1";
+        document.getElementById("u1").innerHTML = "Accelerate Num1 (Currently " + d.Num1pc.toFixed(2) + "/click) Cost:" + d.u1c.toFixed(2) + "Num1";
     }
 }
 var loop = window.setInterval(function() {
