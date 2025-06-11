ba6da62c4527b6bd7a300e461a5a31b96e8efb32
LaylaWolfsS <laylawolfss@gmail.com>
2025-06-11 06:17:10 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
+var c = new Array(8);
+for (let ci = 0; ci < c.length; ci++) {
+    c[ci] = Math.pow(10, 3*ci+1);
+}
+// TODO: Rewire this so u1e is its own effect and not the base of u1
 let d = {
     Num1: 0,
     Num1pc: 1,
     u1: 0,
-    u1c: 10,
+    u1c: c[0],
+    u1e: 1.1,
+    u2: 0,
+    u2c: c[1],
+    u2e: 0
 }  
 function incNum1() {
-    d.Num1 += d.Num1pc;
-    document.getElementById("Num1").innerHTML = Math.round(d.Num1) + " Num1";
+    d.Num1 = parseFloat((d.Num1 + d.Num1pc).toFixed(3));
+    document.getElementById("Num1").innerHTML = d.Num1 + " Num1";
 }
 function buyu1() {
     if(d.Num1 >= d.u1c) {
-        d.Num1 -= parseFloat(d.u1c.toFixed(2));
-        d.Num1pc = parseFloat((d.Num1pc*1.1).toFixed(2));
-        d.u1c = parseFloat((d.u1c*1.2).toFixed(2));
-        d.u1 = parseFloat((d.u1 + 1).toFixed(2));
-        document.getElementById("u1a").innerHTML = "Level " + Math.round(d.u1);
-        document.getElementById("Num1").innerHTML = Math.round(d.Num1) + " Num1";
-        document.getElementById("u1").innerHTML = "Accelerate Num1 (Currently " + Math.round(d.Num1pc) + "/click) Cost:" + Math.round(d.u1c) + "Num1";
+        d.Num1 -= parseFloat(d.u1c.toFixed(3));
+        d.u1c = parseFloat((d.u1c*1.15).toFixed(3));
+        d.u1 = parseFloat((d.u1 + 1).toFixed(3));
+        document.getElementById("u1a").innerHTML = "Level " + d.u1;
+        document.getElementById("Num1").innerHTML = d.Num1 + " Num1";
+        document.getElementById("u1").innerHTML = "Multiply Num1 gain by " + d.u1e + " (Currently " + d.Num1pc + "/click) Cost:" + d.u1c + "Num1";
+    }
+}
+function buyu2() {
+    if(d.Num1 >= d.u2c) {
+        d.Num1 -= parseFloat(d.u2c.toFixed(3));
+        d.u1 = parseFloat((d.u1 + 2).toFixed(3));
+        d.u2c = parseFloat((d.u2c*2).toFixed(3));
+        d.u2 = parseFloat((d.u2 + 1).toFixed(3));
+        d.u1e = parseFloat((d.u1e + 0.001).toFixed(3));
+        d.u2e = parseFloat((d.u2e + 0.001).toFixed(3));
+        document.getElementById("u2a").innerHTML = "Level " + d.u2;
+        document.getElementById("u2").innerHTML = "Add 0.001 to the base of the first upgrade (Currently +" + d.u2e + ") Cost:" + d.u2c + "Num1";
     }
 }
 var loop = window.setInterval(function() {
@@ -24,8 +44,11 @@ var loop = window.setInterval(function() {
 }, 1000);
 var saveloop = window.setInterval(function() {
     localStorage.setItem("Data", JSON.stringify(d));
-}, 20000);
+}, 10000);
 var save = JSON.parse(localStorage.getItem("Data"));
 if(save !== null) {
     d = save;
 }
+var effectloop = window.setInterval(function() {
+    d.Num1pc = parseFloat((Math.pow(d.u1e,d.u1)));
+}, 50);
