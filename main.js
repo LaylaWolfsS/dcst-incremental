ccb1bc2caa4d5f5d29dc678996ad1023ad28a0d2
LaylaWolfsS <laylawolfss@gmail.com>
2025-06-12 08:05:58 -0700

    3rd upgrade less gooo

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ let d = {
     u2: 0,
     u2c: c[1],
     u2e: 0,
+    u2r: 2,
     u2s: s[0],
     u3: 0,
     u3c: c[2],
@@ -55,7 +56,7 @@ function buyu1() {
 function buyu2() {
     if(d.Num1 >= d.u2c) {
         d.Num1 -= parseFloat(d.u2c.toFixed(3));
-        d.u2c *= 2;
+        d.u2c *= d.u2r;
         d.u2 += 1;
         d.u1e += 0.004;
         d.u2e += 0.004;
@@ -67,16 +68,18 @@ function buyu3() {
         d.Num1 -= parseFloat(d.u3c.toFixed(3));
         d.u3c *= 2;
         d.u3 += 1;
-        d.u1c /= 2;
+        d.u1c /= 1;
     }
 }
 var loop = window.setInterval(function() {
     incNum1();
-    document.getElementById("Num1").innerHTML = parseFloat(d.Num1.toFixed(3)) + " Num1";
+    document.getElementById("Num1").innerHTML = "You have" + parseFloat(d.Num1.toFixed(3)) + " Fabric";
     document.getElementById("u1a").innerHTML = "Level " + parseFloat(d.u1.toFixed(3));
-    document.getElementById("u1").innerHTML = "Multiply Num1 gain by " + parseFloat(d.u1e.toFixed(3)) + " (Currently " + parseFloat(d.Num1pc.toFixed(3)) + "/click) Cost: " + parseFloat(d.u1c.toFixed(3)) + " Num1";
+    document.getElementById("u1").innerHTML = "Multiply Fabric gain by " + parseFloat(d.u1e.toFixed(3)) + " (Currently " + parseFloat(d.Num1pc.toFixed(3)) + "/tick) Cost: " + parseFloat(d.u1c.toFixed(3)) + " Fabric";
     document.getElementById("u2a").innerHTML = "Level " + parseFloat(d.u2.toFixed(3));
-    document.getElementById("u2").innerHTML = "Add 0.004 to the base of the first upgrade (Currently +" + parseFloat(d.u2e.toFixed(3)) + ") Cost: " + parseFloat(d.u2c.toFixed(3)) + " Num1";
+    document.getElementById("u2").innerHTML = "Add 0.004 to the base of the first upgrade (Currently +" + parseFloat(d.u2e.toFixed(3)) + ") Cost: " + parseFloat(d.u2c.toFixed(3)) + " Fabric";
+    document.getElementById("u3a").innerHTML = "Level " + parseFloat(d.u3.toFixed(3));
+    document.getElementById("u3").innerHTML = "Divide the scaling of all previous upgrades by 1.01 (Currently /" + parseFloat(d.u3e.toFixed(3)) + ") Cost: " + parseFloat(d.u3c.toFixed(3)) + " Fabric";
 }, 50);
 var saveloop = window.setInterval(function() {
     localStorage.setItem("Data", JSON.stringify(d));
