ff68932c333e79703930a61e32abfee1635551c9
LaylaWolfsS <laylawolfss@gmail.com>
2025-08-22 14:12:22 -0700

    pushing my buttons

---
+++ button.js
@@ -0,0 +1,6 @@
+function close(n) {
+    eval("document.getElementById(" + n + ").style.display = 'none'");
+}
+function open(n) {
+    eval("document.getElementById(" + n + ").style.display = 'block'");
+}
