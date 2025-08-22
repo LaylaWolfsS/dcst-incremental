7c446f510a0ee2f1a7355c281461bbe9d260b7e9
LaylaWolfsS <laylawolfss@gmail.com>
2025-08-22 14:53:45 -0700

    Add files via upload

--- button.js
+++ button.js
@@ -1,6 +1,7 @@
+const t = ["Fabric", "Quarks"];
 function close(n) {
-    eval("document.getElementById(" + n + ").style.display = 'none'");
+    eval("document.getElementById(" + t[n] + ").style.display = 'none'");
 }
 function open(n) {
-    eval("document.getElementById(" + n + ").style.display = 'block'");
+    eval("document.getElementById(" + t[n] + ").style.display = 'block'");
 }
