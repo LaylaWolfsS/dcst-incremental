59e57ec357ed481ec5b80bbd887b633ec04847d7
LaylaWolfsS <laylawolfss@gmail.com>
2025-08-23 12:26:21 -0700

    fixed tab layout

--- button.js
+++ button.js
@@ -1,7 +1,7 @@
 const t = ["Fabric", "Quarks"];
 function close(n) {
-    eval("document.getElementById(" + t[n] + ").style.display = 'none'");
+    eval("document.getElementByClassName(" + t[n] + ").style.display = 'none'");
 }
 function open(n) {
-    eval("document.getElementById(" + t[n] + ").style.display = 'block'");
+    eval("document.getElementByClassName(" + t[n] + ").style.display = 'block'");
 }
