680916bee25096e2d6d81124687c4047c0507d3f
LaylaWolfsS <laylawolfss@gmail.com>
2025-08-25 08:55:46 -0700

    it came as 1

--- button.js
+++ button.js
@@ -1,7 +1,7 @@
 const t = ["Fabric", "Quarks"];
 function close(n) {
-    eval("document.getElementByClassName(" + t[n] + ").style.display = 'none'");
+    eval("document.getElementsByClassName(" + t[n] + ").style.display = 'none'");
 }
 function open(n) {
-    eval("document.getElementByClassName(" + t[n] + ").style.display = 'block'");
+    eval("document.getElementsByClassName(" + t[n] + ").style.display = 'block'");
 }
