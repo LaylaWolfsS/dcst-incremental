71b090abe5386c482b9765711ba756dda86d52c6
LaylaWolfsS <laylawolfss@gmail.com>
2025-09-03 08:58:55 -0700

    aheter

--- button.js
+++ button.js
@@ -1,7 +1,12 @@
 const t = ["Fabric", "Quarks"];
+var u;
 function close(n) {
-    eval("document.getElementsByClassName(" + t[n] + ").style.display = 'none'");
+    eval("u = document.getElementsByClassName(" + t[n] + ");");
+    eval("for(let v = 0; v < u.length; v++) u[v].style.display = 'none'");
+    u = [];
 }
 function open(n) {
-    eval("document.getElementsByClassName(" + t[n] + ").style.display = 'block'");
+    eval("u = document.getElementsByClassName(" + t[n] + ");");
+    eval("for(let v = 0; v < u.length; v++) u[v].style.display = 'block'");
+    u = [];
 }
